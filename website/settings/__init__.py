bfb28e47c2aab58821d76f5473133a4ad154a69a
Rishabh Kharbanda <rishabhkharbanda@Macbooks-MacBook-Air.local>
2017-10-15 00:51:30 +0530

    a

--- website/settings/__init__.py
+++ website/settings/__init__.py
@@ -1,8 +1,2 @@
-from .base import *
 
 from .production import *
-
-try:
-   from .local import *
-except:
-   pass
--- website/settings/production.py
+++ website/settings/production.py
@@ -70,7 +70,9 @@ MANAGERS = ADMINS
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +102,6 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # DATABASES['default']['CONN_MAX_AGE'] = 500
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
