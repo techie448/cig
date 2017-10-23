2064587a35006d13398962a5d0108e92007db49a
techie448 <techie448@gmail.com>
2017-10-24 02:12:57 +0530

    Update production.py

--- website/settings/production.py
+++ website/settings/production.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
