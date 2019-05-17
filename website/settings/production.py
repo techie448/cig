07f914389954d4ec38a8ceb0bef963aed24f10ba
rishabhkharbanda <techie448@gmail.com>
2019-05-17 02:57:07 -0400

    commit static dirs

--- website/settings/production.py
+++ website/settings/production.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
