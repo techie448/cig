09a40a1a3fab4fc27a48094f810ed3d923259a05
rishabhkharbanda <techie448@gmail.com>
2019-05-17 03:00:42 -0400

    commit static dirs

--- website/settings/production.py
+++ website/settings/production.py
@@ -138,14 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 CORS_REPLACE_HTTPS_REFERER      = True
 HOST_SCHEME                     = "https://"
 SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
